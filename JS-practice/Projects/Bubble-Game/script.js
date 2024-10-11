e6707e1e77788d60ea8f4c7c311bfa135e0fdd10
Celestial-MohitTayal <mohit.tayal@celestialsys.com>
2024-10-11 16:37:47 +0530

    Bubble Game Complete

--- JS-practice/Projects/Bubble-Game/script.js
+++ JS-practice/Projects/Bubble-Game/script.js
@@ -1,3 +1,13 @@
+
+var timer = 60;
+var score = 0;
+var rn = 0;
+
+function increaseScore() {
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
+}
+
 function makeBubble() {
   let clutter = "";
 
@@ -8,26 +18,35 @@ function makeBubble() {
   document.querySelector("#pbtm").innerHTML = clutter;
 }
 
-
 function runTimer() {
-    var timer = 60;
     var timerInt = setInterval(function () {
       if (timer > 0) {
         timer--;
         document.querySelector("#timerval").textContent = timer;
       } else {
         clearInterval(timerInt);
+        document.querySelector('#pbtm').innerHTML = `<h1>Game Over!</h1>`
       }
     }, 1000);
   }
-
 
 function makeHit() {
     rn = Math.floor(Math.random()*10);
     document.querySelector("#hitval").textContent = rn;
 }
 
+document.querySelector("#pbtm").addEventListener("click", function(dets) {
+    var clickedNumber = Number(dets.target.textContent)
+
+    if(clickedNumber == rn) {
+        increaseScore();
+        makeBubble();
+        makeHit();
+    }
+})
+
 
 makeBubble();
 runTimer();
 makeHit();
+increaseScore()
