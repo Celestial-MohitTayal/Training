64664ee7226d431ad8cd6e733f7661d0e273f239
Celestial-MohitTayal <mohit.tayal@celestialsys.com>
2024-10-25 13:43:59 +0530

    Java-Script Assignment-3(LocalStorage Done)

--- JavaScript/Assignments/Assignment-3/script.js
+++ JavaScript/Assignments/Assignment-3/script.js
@@ -2,29 +2,43 @@ const getUser = async () => {
     try {
       const response = await fetch('https://jsonplaceholder.typicode.com/users');
       const userArray = await response.json();
+
+      console.log(userArray);
+
+      let stringifiedUserArray = JSON.stringify(userArray);
+      localStorage.setItem('array', stringifiedUserArray)
+      let arrayFromStorage = localStorage.getItem('array');
+      let arrayParsed = JSON.parse(arrayFromStorage);
+
+      console.log(arrayParsed);
   
       let temp = '';
-  
-      userArray.map((user, index) => {
+      arrayParsed.map((user, index) => {
         temp += `<tr
-                  class="bg-white border-b"
-                >
-                  <th
-                    scope="row"
-                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
-                  >
-                    <button class="font-medium text-blue-600 dark:text-blue-500 hover:underline" onclick="getCharDetails(${
-                      index + 1
-                    })">${user?.name ?? 'N/A'}</button>
-                  </th>
-                  <td class="px-6 py-4">${user?.username ?? 'Unknown'}</td>
-                  <td class="px-6 py-4">${user?.email ?? 'Unknown'}</td>
-                  <td class="px-6 py-4">${user?.address?.suite ?? 'Unknown'}, ${user?.address?.street ?? 'Unknown'}, ${user?.address?.city ?? 'Unknown'}, ${user?.address?.zipcode ?? 'Unknown'} </td>
-                  <td class="px-6 py-4">${user?.phone ?? 'Unknown'}</td>
-                  <td class="px-6 py-4">${user?.website ?? 'Unknown'}</td>
-                  <td class="px-6 py-4">${user?.company?.name ?? 'Unknown'}</td>
-                </tr>`;
-        document.getElementById('userDetails').innerHTML = temp;
+        class="bg-white border-b"
+      >
+        <th
+          scope="row"
+          class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+        >
+          <button class="font-medium text-blue-600 dark:text-blue-500 hover:underline" onclick="getCharDetails(${
+            index + 1
+          })">${user?.name ?? 'N/A'}</button>
+        </th>
+        <td class="px-6 py-4">${user?.username ?? 'Unknown'}</td>
+        <td class="px-6 py-4">${user?.email ?? 'Unknown'}</td>
+        <td class="px-6 py-4">${user?.address?.suite ?? 'Unknown'}, ${user?.address?.street ?? 'Unknown'}, ${user?.address?.city ?? 'Unknown'}, ${user?.address?.zipcode ?? 'Unknown'} </td>
+        <td class="px-6 py-4">${user?.phone ?? 'Unknown'}</td>
+        <td class="px-6 py-4">${user?.website ?? 'Unknown'}</td>
+        <td class="px-6 py-4">${user?.company?.name ?? 'Unknown'}</td>
+        <td class="px-6 py-4 text-right">
+           <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+        </td>
+        <td class="px-6 py-4 text-right">
+           <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
+        </td>
+        </tr>`;
+      document.getElementById('userDetails').innerHTML = temp;
       });
     } catch (error) {
       console.error('Error fetching character data:', error);
@@ -34,3 +48,6 @@ const getUser = async () => {
   window.onload = function () {
     getUser();
   };
+
+
+
