46eac77a8f952a8e61140626c1028367aa946066
Celestial-MohitTayal <mohit.tayal@celestialsys.com>
2024-10-30 23:32:20 +0530

    React-Branch React Assignments(Styling Done)

---
+++ React/Assignments/assignment-1/src/components/fetchData.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../data.css'
+
+class FetchData extends React.Component{
+    render()
+    {
+        return (
+            <section>
+            <h1>Github Profile</h1>
+            </section>
+        )
+    }
+}
+
+export default FetchData;
---
+++ React/Assignments/assignment-1/src/components/showData.jsx
@@ -0,0 +1,15 @@
+export default function ShowData() {
+    return (
+        <section className="header">
+            <button>Show Data</button>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Name:</th>
+                        <td>Mohit</td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+    )
+}
