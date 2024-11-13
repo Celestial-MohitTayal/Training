7891b23e626aec12eae74a34a68d2e7a792d54d9
Celestial-MohitTayal <mohit.tayal@celestialsys.com>
2024-11-13 12:27:28 +0530

    React-Branch React Redux react-rtk (Redux in React )

---
+++ React/Redux/react-rtk/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import IcecreamView from './features/icecream/icecreamView'
+import CakeView from './features/cake/CakeView'
+import UserView from './features/user/userView'
+
+function App() {
+
+  return (
+    <>
+     <CakeView />
+     <IcecreamView />
+     <UserView />
+    </>
+  )
+}
+
+export default App
